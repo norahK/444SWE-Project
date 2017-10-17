7aa8adc9b221bc35823aaf819b678e519fc4060f
norahK <alnoor0416@gmail.com>
2017-10-17 13:30:04 +0300

    database file

---
+++ secript.js
@@ -0,0 +1,14 @@
+
+<script src="https://www.gstatic.com/firebasejs/4.5.2/firebase.js"></script>
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyASKRKyNwRsaj9FfQCzkAhUCjDVmKhzNgA",
+    authDomain: "pick-a-dish.firebaseapp.com",
+    databaseURL: "https://pick-a-dish.firebaseio.com",
+    projectId: "pick-a-dish",
+    storageBucket: "pick-a-dish.appspot.com",
+    messagingSenderId: "362598252651"
+  };
+  firebase.initializeApp(config);
+</script>
