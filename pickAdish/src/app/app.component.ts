2599a3b58aeac1bbdfe7871d93c7d27edcde701d
norahK <alnoor0416@gmail.com>
2017-10-24 13:50:58 +0300

    g

--- pickAdish/src/app/app.component.ts
+++ pickAdish/src/app/app.component.ts
@@ -10,7 +10,11 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 })
 export class MyApp {
   //home page
-  rootPage:any;// ='LoginPage';// TabsPage;
+<<<<<<< HEAD
+  rootPage:any ='LoginPage';// TabsPage;
+=======
+  rootPage:'LoginPage';// TabsPage;
+>>>>>>> ae0c9a01c2dd149f0294f889807ba591034bc29a
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
---
+++ pickAdish/src/providers/dish-page/dish-page.service.ts
@@ -0,0 +1,21 @@
+
+import {Injectable} from '@angular/core;
+import {AngularFireDatabase} from 'angularfire2/database';
+
+@Injectable()
+export class DishPageService{
+	
+private dishPageRef = this.db.dishes'dish-page'
+
+	constructor(private db: AngularFireDatabase){}
+
+
+
+	getDishPage(){
+
+     return this.dishPageRef;
+
+	}
+
+
+}
