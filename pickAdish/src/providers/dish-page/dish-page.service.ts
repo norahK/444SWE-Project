af67eb76bdc49912e2f61cb67f6954d0ee8a6df7
norahK <alnoor0416@gmail.com>
2017-10-24 14:13:46 +0300

    g

--- pickAdish/src/providers/dish-page/dish-page.service.ts
+++ pickAdish/src/providers/dish-page/dish-page.service.ts
@@ -4,16 +4,19 @@ import {AngularFireDatabase} from 'angularfire2/database';
 
 @Injectable()
 export class DishPageService{
-	
-private dishPageRef = this.db.dishes'dish-page';
 
-	constructor(private db: AngularFireDatabase){}
+//private dishPageRef = this.db.dishes'dish-page';
+
+	constructor(private db: AngularFireDatabase){
+
+
+  }
 
 
 
 	getDishPage(){
 
-     return this.dishPageRef;
+   ///  return this.dishPageRef;
 
 	}
 
