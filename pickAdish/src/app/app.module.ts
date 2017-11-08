93b0190dc2651b692624f54417beea50ccaff702
norahK <alnoor0416@gmail.com>
2017-11-08 16:51:22 +0300

    h

--- pickAdish/src/app/app.module.ts
+++ pickAdish/src/app/app.module.ts
@@ -31,14 +31,7 @@ import {IonRating} from '../components/ion-rating/ion-rating';
     ShopsPage,
     TabsPage,
    LoginPage,
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-=======
->>>>>>> 6a9e5d05d8e68ed05f27a8b7a9c34ad9b430c1f0
    IonRating
-   //RegisterPage,
->>>>>>> 6a9e5d05d8e68ed05f27a8b7a9c34ad9b430c1f0
 
   ],
   imports: [
