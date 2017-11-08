6a9e5d05d8e68ed05f27a8b7a9c34ad9b430c1f0
Asmaa <asmaa.alrubia@gmail.com>
2017-11-08 16:44:53 +0300

    +

--- pickAdish/src/app/app.component.ts
+++ pickAdish/src/app/app.component.ts
@@ -10,11 +10,11 @@ import { TabsPage } from '../pages/tabs/tabs';
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any='DishPage';// TabsPage;
+  rootPage:any;//='DishPage';// TabsPage;
 
   constructor(    private authr:AngularFireAuth,
     platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
-/*   platform.ready().then(() => {
+  platform.ready().then(() => {
       //Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
      statusBar.styleDefault();
@@ -28,7 +28,7 @@ export class MyApp {
          unsubscribe();
       }
     });
-    }); */
+    });
 }
 
 }
