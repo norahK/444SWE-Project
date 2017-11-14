68de4c99581e71d2e9d8ee9f12765dc70d534288
norahK <alnoor0416@gmail.com>
2017-11-14 16:29:49 +0300

    u

--- pickAdish/src/pages/add-new-dish/add-new-dish.ts
+++ pickAdish/src/pages/add-new-dish/add-new-dish.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-import {AlertController, IonicPage, NavController, NavParams } from 'ionic-angular';
+import {LoadingController,AlertController, IonicPage, NavController, NavParams } from 'ionic-angular';
 import{storage}from 'firebase';
 import{Camera,CameraOptions}from'@ionic-native/camera';
 import{AngularFireDatabase}from 'angularfire2/database';
 import * as firebase from 'firebase/app';
 import { Dish } from '../../models/dish';
+import { Observable } from 'rxjs/Observable';
 
 @IonicPage()
 @Component({
@@ -13,7 +14,7 @@ import { Dish } from '../../models/dish';
 })
 export class AddNewDishPage {
   types: string = "non";
-  shops:any;
+  shops:Observable<any>;;
   cshop :string ="non";
 dish={} as Dish;
   //thisdish :Dish ;
@@ -24,7 +25,8 @@ testCheckboxOpen = false;
 testCheckboxResult: any;
   constructor(public db: AngularFireDatabase,
     public alertCtrl: AlertController,
-    private cam:Camera,
+        public loadingCtrl: LoadingController,
+      private cam:Camera,
     public navCtrl: NavController,
      public navParams: NavParams) {
   this.shoppath = db.database.ref('shops');
@@ -33,12 +35,13 @@ testCheckboxResult: any;
   //$('#checkboxes input:checked').each(function() {
    //   selected.push($(this).attr('name'));
   //});
+  this.shops=this.db.list('shops').valueChanges();
+
   }
 
   ionViewDidLoad() {
   }
 async takePhoto(){
-
   try{
 const options: CameraOptions={
   quality:50,
@@ -62,34 +65,29 @@ addShop(){
 
   let prompt = this.alertCtrl.create({
     title: 'add new shop',
-    message: "Enter a name ,...",
+    message: "Enter a name ",
     inputs: [
       {
         name: 'name',
-        placeholder: 'Title'
+        placeholder: 'shop name'
       },
-      {
-        name: 'a',
-        placeholder: 'Title'
-      },{
-        name: 'b',
-        placeholder: 'Title'
-      },
+
     ],
     buttons: [
       {
         text: 'Cancel',
         handler: data => {
-
-          console.log('Cancel clicked');
+          prompt.dismiss();
         }
       },
       {
         text: 'Save',
         handler: data => {
           console.log('Saved clicked');
-          const id= this.dishpath.push();   //get id then add name loc and .. in saide the id s
-          this.shoppath.child(id).set({name:data.name});
+          const id= this.dishpath.push().getKey();   //get id then add name loc and .. in saide the id s
+          this.shoppath.child(id).child('name').set(data.name);
+          this.shoppath.child(id).child('id').set(id);
+          this.dish.shop=id;
         }
       }
     ]
@@ -99,13 +97,37 @@ addShop(){
 
 }
 submit(dish:Dish){
+  if(dish.name==null && dish.price==0){
+    this.Loading('you must add name and price');
+    return;
+  }
+  else{
 //this.thisdish.name=;
-this.dishpath.push(dish);
-//sucsuss or error loading before toast
+this.dishpath.push(dish).then((success)=>{this.Loading('added');
 //or add othe +cancel
 this.back();
+},(Error)=>{
+  this.Loading('error');
 
+});
+  }
 }
+Loading(message) {
+  const loading = this.loadingCtrl.create({
+    duration: 500
+  });
+  loading.onDidDismiss(() => {
+    const alert = this.alertCtrl.create({
+      subTitle: message,
+      buttons: ['Dismiss']
+    });
+    alert.present();
+  });
+
+  loading.present();
+}
+
+
 back(){
   this.navCtrl.pop();
    // window.history.back();
@@ -118,20 +140,20 @@ doCheckbox() {
   alert.addInput({
     type: 'checkbox',
     label: 'meeting',
-    value: 'value1',
+    value: 'meeting',
     checked: true
   });
 
   alert.addInput({
     type: 'checkbox',
     label: 'party',
-    value: 'value2'
+    value: 'party'
   });
 
   alert.addInput({
     type: 'checkbox',
     label: 'office',
-    value: 'value3'
+    value: 'office'
   });
 
 
@@ -139,6 +161,7 @@ doCheckbox() {
   alert.addButton({
     text: 'Okay',
     handler: (data: any) => {
+      ///what is this data
       this.dish.occasion= data;
     }
   });
